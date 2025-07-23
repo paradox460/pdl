00000003a9b522d372282a8e5067e458c9ed099f
Jeff Sandberg <jeff@pdx.su>
2025-07-23 15:42:31 -0600

    Set up mix project

--- mix.exs
+++ mix.exs
@@ -1,20 +1,44 @@
 defmodule Pdl.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/paradox460/pdl"
+
   def project do
     [
       app: :pdl,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      source_url: @source_url,
+      homepage_url: @source_url,
+      name: "Pdl",
+      description: "A Process Dictionary based loader library",
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Jeff Sandberg"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: @source_url
+      },
+      files: ~w[
+        lib
+        test
+        mix.exs
+        README.md
+        LICENSE
+      ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      # extra_applications: [:logger]
     ]
   end
 
